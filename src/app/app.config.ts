57c69a87df6e373f108da90965a26b69d6e18ed9
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-20 17:58:46 +0200

    feat: integrate PrimeNG button and update application configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,18 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNg } from './prime-ng/prime-ng.provider';
+import { providePrimeNG } from 'primeng/config';
+import  Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    providePrimeNg()
-  ]
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+  ],
 };
