f9ad9e030fd5a02ed175ec738b433f9ab4d1b52d
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-20 19:48:19 +0200

    feat: enhance character creator with step panels and class selection component

--- src/app/features/character-creator/pages/character-creator/character-creator.component.ts
+++ src/app/features/character-creator/pages/character-creator/character-creator.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { StepperModule } from 'primeng/stepper';
 import { CreationSteps } from '../../../character-viewer/dictionaries/creation-steps.dictionary';
+import { ButtonModule } from 'primeng/button';
+import { ClassSelectionComponent } from "../../components/class-selection/class-selection.component";
 
 @Component({
   selector: 'app-character-creator',
-  imports: [StepperModule],
+  imports: [StepperModule, ButtonModule, ClassSelectionComponent],
   templateUrl: './character-creator.component.html',
   styleUrl: './character-creator.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
