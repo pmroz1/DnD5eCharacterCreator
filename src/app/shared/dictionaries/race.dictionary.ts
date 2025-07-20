660f851c4e61634e9d981a1aae56656fcb9fa2ed
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-20 18:48:35 +0200

    feat: add race dictionary enum for character species

---
+++ src/app/shared/dictionaries/race.dictionary.ts
@@ -0,0 +1,14 @@
+// https://www.dndbeyond.com/species
+ export const enum RACE_DICTIONARY {
+    Aasimar = 'Aasimar',
+    Dragonborn = 'Dragonborn',
+    Dwarf = 'Dwarf',
+    Elf = 'Elf',
+    Gnome = 'Gnome',
+    Goliath = 'Goliath',
+    Halfling = 'Halfling',
+    HalfElf = 'Half-Elf',
+    Human = 'Human',
+    Orc = 'Orc',
+    Tiefling = 'Tiefling',
+ }
