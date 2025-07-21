013bfcf114c20efcd38f3905969beb250fc2bbab
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-21 19:31:10 +0200

    feat: enhance class selection component with dynamic titles and descriptions; add link pre-note

--- src/app/features/character-creator/components/class-selection/class-selection.component.ts
+++ src/app/features/character-creator/components/class-selection/class-selection.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
+import { CreationStepsTitles, CreationStepsDescriptions, CreationSteps, LinkPreNote } from '../../dictionaries/creation-steps.dictionary';
+import { TipsLookup } from '../../../../shared/dictionaries/tips-lookup.dictionary';
 
 @Component({
   selector: 'app-class-selection',
@@ -8,4 +10,10 @@ import { CardModule } from 'primeng/card';
   styleUrl: './class-selection.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ClassSelectionComponent { }
+export class ClassSelectionComponent {
+  CreationSteps = CreationSteps;
+  CreationStepsTitles = CreationStepsTitles;
+  CreationStepsDescriptions = CreationStepsDescriptions;
+  LinkPreNote = LinkPreNote;
+  TipsLookup = TipsLookup;
+}
--- src/app/features/character-creator/dictionaries/creation-steps.dictionary.ts
+++ src/app/features/character-creator/dictionaries/creation-steps.dictionary.ts
@@ -6,3 +6,23 @@ export enum CreationSteps {
     Alignment = 'alignment',
     Details = 'details',
 }
+
+export const CreationStepsTitles = {
+    [CreationSteps.ClassSelection]: 'Class Selection',
+    [CreationSteps.RaceSelection]: 'Race Selection',
+    [CreationSteps.OriginSelection]: 'Origin Selection',
+    [CreationSteps.AbilityScores]: 'Ability Scores',
+    [CreationSteps.Alignment]: 'Alignment',
+    [CreationSteps.Details]: 'Details',
+};
+
+export const CreationStepsDescriptions = {
+    [CreationSteps.ClassSelection]: 'Choose your character\'s class.',
+    [CreationSteps.RaceSelection]: 'Select your character\'s race.',
+    [CreationSteps.OriginSelection]: 'Choose your character\'s origin.',
+    [CreationSteps.AbilityScores]: 'Assign your character\'s ability scores.',
+    [CreationSteps.Alignment]: 'Select your character\'s alignment.',
+    [CreationSteps.Details]: 'Provide additional details about your character.',
+};
+
+export const LinkPreNote = 'For more information, visit:';
