4228a46713919ecd3001a87aebfc1e0039825173
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-24 19:53:25 +0200

    feat: refactor carousel component for improved readability and update responsive options

--- src/app/shared/components/carousel/carousel.component.ts
+++ src/app/shared/components/carousel/carousel.component.ts
@@ -1,6 +1,13 @@
-import { ChangeDetectionStrategy, Component, input, OnInit, signal, ViewEncapsulation } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    input,
+    OnInit,
+    signal,
+    ViewEncapsulation,
+} from '@angular/core';
 import { CarouselModule } from 'primeng/carousel';
-import { ImageCardComponent } from "../image-card/image-card.component";
+import { ImageCardComponent } from '../image-card/image-card.component';
 import { ImageCardData } from '../image-card/models/image-card.model';
 
 @Component({
@@ -9,18 +16,26 @@ import { ImageCardData } from '../image-card/models/image-card.model';
     imports: [CarouselModule, ImageCardComponent],
     template: `
         @if(items().length>0){
-        <p-carousel
-            [value]="items()"
-            [numVisible]="numVisible()"
-            [responsiveOptions]="responsiveOptions()"
-            [numScroll]="3"
-            [circular]="true"
-            class="mt-5 mb-5"
-        >
-            <ng-template let-item #item class="p-carousel-item m-20">
-                 <app-image-card [cardData]="item" (cardClick)="onCardClicked($event)" [isClicked]="clickedItem()?.title === item.title"></app-image-card>
-            </ng-template>
-        </p-carousel>
+        <div class="w-full">
+            <p-carousel
+                [value]="items()"
+                [numVisible]="numVisible()"
+                [responsiveOptions]="responsiveOptions()"
+                [numScroll]="3"
+                [circular]="true"
+                class="mt-5 mb-5"
+            >
+                <ng-template let-item #item>
+                    <div class="p-2">
+                        <app-image-card
+                            [cardData]="item"
+                            (cardClick)="onCardClicked($event)"
+                            [isClicked]="clickedItem()?.title === item.title"
+                        ></app-image-card>
+                    </div>
+                </ng-template>
+            </p-carousel>
+        </div>
         } @else{
         <div class="text-center text-gray-500 dark:text-gray-400">No items available</div>
         }
@@ -37,32 +52,42 @@ export class CarouselComponent {
     readonly numVisible = input<number>(9);
     readonly responsiveOptions = input<any[]>([
         {
-            breakpoint: '1920px',
+            breakpoint: '2800px',
             numVisible: 6,
+            numScroll: 3,
+        },
+        {
+            breakpoint: '1920px',
+            numVisible: 5,
+            numScroll: 3,
         },
         {
             breakpoint: '1600px',
-            numVisible: 5,
+            numVisible: 4,
+            numScroll: 3,
         },
         {
             breakpoint: '1200px',
-            numVisible: 4,
+            numVisible: 3,
+            numScroll: 2,
         },
         {
             breakpoint: '1024px',
-            numVisible: 3,
+            numVisible: 2,
+            numScroll: 2,
         },
         {
             breakpoint: '768px',
             numVisible: 2,
+            numScroll: 1,
         },
         {
             breakpoint: '560px',
             numVisible: 1,
+            numScroll: 1,
         },
     ]);
     clickedItem = signal<ImageCardData | null>(null);
-
 
     onCardClicked($event: ImageCardData | null): void {
         console.log('Card clicked:', $event);
