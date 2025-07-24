21642a8419c3d28803c3085b2ba16d9d2ee145a3
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-24 20:02:36 +0200

    feat: update import paths in class selection component and tsconfig for consistency

--- src/app/features/character-creator/components/class-selection/class-selection.component.ts
+++ src/app/features/character-creator/components/class-selection/class-selection.component.ts
@@ -1,25 +1,30 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CardModule } from 'primeng/card';
-import { CreationStepsTitles, CreationStepsDescriptions, CreationSteps, LinkPreNote } from '../../dictionaries/creation-steps.dictionary';
-import { TipsLookup } from '../../../../shared/dictionaries/tips-lookup.dictionary';
-import { CarouselComponent } from "../../../../shared/components/carousel/carousel.component";
-import { ALL_CLASSES } from '../../../data/classes.data';
-import { CharacterCreatorService } from '../../services/character-creator.service';
+import {
+    CreationStepsTitles,
+    CreationStepsDescriptions,
+    CreationSteps,
+    LinkPreNote,
+} from '@features/character-creator/dictionaries/creation-steps.dictionary';
+import { TipsLookup } from '@shared/dictionaries/tips-lookup.dictionary';
+import { CarouselComponent } from '@shared/components/carousel/carousel.component';
+import { ALL_CLASSES } from '@data/classes.data';
+import { CharacterCreatorService } from '@features/character-creator/services/character-creator.service';
 
 @Component({
-  selector: 'app-class-selection',
-  imports: [CardModule, CarouselComponent],
-  templateUrl: './class-selection.component.html',
-  styleUrl: './class-selection.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-class-selection',
+    imports: [CardModule, CarouselComponent],
+    templateUrl: './class-selection.component.html',
+    styleUrl: './class-selection.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ClassSelectionComponent {
-  characterCreatorService = inject(CharacterCreatorService);
-  CreationSteps = CreationSteps;
-  CreationStepsTitles = CreationStepsTitles;
-  CreationStepsDescriptions = CreationStepsDescriptions;
-  LinkPreNote = LinkPreNote;
-  TipsLookup = TipsLookup;
+    characterCreatorService = inject(CharacterCreatorService);
+    CreationSteps = CreationSteps;
+    CreationStepsTitles = CreationStepsTitles;
+    CreationStepsDescriptions = CreationStepsDescriptions;
+    LinkPreNote = LinkPreNote;
+    TipsLookup = TipsLookup;
 
-  carouselItems = this.characterCreatorService.convertClassesToImageCards(ALL_CLASSES);
+    carouselItems = this.characterCreatorService.convertClassesToImageCards(ALL_CLASSES);
 }
