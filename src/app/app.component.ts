1983dc9771740cccc3c500aa36ca039e5c971426
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-25 22:09:23 +0200

    fix build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuBarComponent } from './shared/menu-bar/menu-bar.component';
+import { MenuBarComponent } from '@shared/components/menu-bar/menu-bar.component';
 
 
 @Component({
