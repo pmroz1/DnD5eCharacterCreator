685f3c510a187ff88e2bed660bd200f55904e217
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-25 21:36:34 +0200

    feat: add ToolProficiencies enum and update Origin model to use it

--- src/app/features/character-creator/models/origin.model.ts
+++ src/app/features/character-creator/models/origin.model.ts
@@ -2,12 +2,13 @@ import { Skills } from '@shared/dictionaries/skills.dictionary';
 import { Ability } from './five-e-class.model';
 import { Feats } from '@shared/dictionaries/feat.dictionary';
 import { BaseRecord } from './base.model';
+import { ToolProficiencies } from '@shared/dictionaries/tool-proficiences.dictionary';
 
 export interface Origin extends BaseRecord {
     abilityScores: Record<Ability, number>[]; // e.g., { Strength: 1, Dexterity: 2 }
     skills: Skills[];
     feats: Feats[];
-    toolProficiencies: string[];
+    toolProficiencies: ToolProficiencies[];
     equipment: {
         guaranteed: string[];
         choices?: {
