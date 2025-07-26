fcbe942e69501a79866c45e61529ac4bb0c0f75a
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-26 23:13:13 +0200

    feat: enhance menu bar with button and dialog for rolling dice

--- src/app/shared/components/menu-bar/menu-bar.component.ts
+++ src/app/shared/components/menu-bar/menu-bar.component.ts
@@ -1,28 +1,47 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
+import { Dialog } from 'primeng/dialog';
 
 @Component({
-  selector: 'app-menu-bar',
-  standalone: true,
-  imports: [MenubarModule],
-  template: `<p-menubar [model]="items"></p-menubar>`,
-  styleUrl: './menu-bar.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-menu-bar',
+    standalone: true,
+    imports: [MenubarModule, ButtonModule, Dialog],
+    template: `<p-menubar [model]="items">
+        <ng-template pTemplate="end"
+            ><p-button label="Roll a dice!" severity="warn" (click)="showDialog()" />
+            <p-dialog
+                header="Header"
+                [modal]="true"
+                [(visible)]="visible"
+                [style]="{ width: '50rem' }"
+            >
+                <p>Dialog content goes here...</p>
+            </p-dialog>
+        </ng-template>
+    </p-menubar>`,
+    styleUrl: './menu-bar.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MenuBarComponent {
-  items: MenuItem[] = [
-    {
-      label: 'New Character',
-      icon: 'pi pi-plus',
-      routerLink: '/character-creator',
-    },
-    {
-      label: 'View Characters',
-      icon: 'pi pi-list',
-      badge: '0',
-      routerLink: '/character-viewer',
-    },
-  ];
+    visible: boolean = false;
 
+    showDialog() {
+        this.visible = !this.visible;
+    }
+
+    items: MenuItem[] = [
+        {
+            label: 'New Character',
+            icon: 'pi pi-plus',
+            routerLink: '/character-creator',
+        },
+        {
+            label: 'View Characters',
+            icon: 'pi pi-list',
+            badge: '0',
+            routerLink: '/character-viewer',
+        },
+    ];
 }
