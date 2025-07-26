9debe5cba900fdcb43d6817b6e28028e7cb266dd
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-26 23:51:47 +0200

    feat: refactor DiceComponent layout for improved structure and styling

--- src/app/shared/dice/dice.component.ts
+++ src/app/shared/dice/dice.component.ts
@@ -7,45 +7,49 @@ import { Button } from 'primeng/button';
     selector: 'app-dice',
     imports: [CardModule, Button],
     template: `<div>
-        <p-card class="w-350 h-auto flex flex-col items-center justify-center">
-            <p-header>D&D Dice Roll</p-header>
-            <p>Roll a dice or <strong>two</strong> to determine your fate!</p>
-            <div
-                class="flex flex-col items-center justify-center mt-4 border-2 border-dashed border-gray-300 rounded-lg p-4 h-50 ml-50 mr-50"
-            >
-                <!-- dices go here  -->
-            </div>
-            <p-button
-                label="Roll Dice"
-                class="mt-4"
-                severity="primary"
-                (click)="rollDice()"
-            ></p-button>
-            <p-button
-                label="Clear"
-                class="mt-2"
-                severity="secondary"
-                (click)="clearRolls()"
-            ></p-button>
-            <div class="flex flex-row items-center justify-center mt-4">
-                @for(dice of dices(); track dice; let idx = $index) {
-                <p-button class="mr-2" severity="secondary">
-                    <p class="text-green-300">D{{ dice }}</p>
-                </p-button>
-                }
-            </div>
-            <div
-                class="flex flex-col items-center justify-center mt-4 border-t-2 rounded border-gray-300 pt-4 h-50 ml-50 mr-50 bg-blue-300"
-            >
-                <p-header>Roll Results</p-header>
-                <p>Here are your roll results:</p>
-            </div>
+        <p-card class="w-350 h-auto ">
+            <div class="flex flex-col items-center justify-center content-center">
+                <p class="font-bold text-5xl">D&D Dice Roll</p>
+                <p>Roll a dice or <strong>two</strong> to determine your fate!</p>
+                <div
+                    class="flex flex-col items-center justify-center mt-4 border-2 border-dashed border-gray-300 rounded-lg p-4 h-50 w-300"
+                >
+                    <!-- dices go here  -->
+                </div>
+                <div class="flex flex-row items-center justify-center mt-4">
+                    <p-button
+                        class="ml-auto mr-2"
+                        label="Roll All"
+                        severity="primary"
+                        (click)="rollDice()"
+                    ></p-button>
+                    <p-button
+                        class="mr-auto ml-2"
+                        label="Clear"
+                        severity="secondary"
+                        (click)="clearRolls()"
+                    ></p-button>
+                </div>
+                <div class="flex flex-row items-center justify-center mt-4">
+                    @for(dice of dices(); track dice; let idx = $index) {
+                    <p-button class="mr-2" severity="secondary">
+                        <p class="text-green-300">D{{ dice }}</p>
+                    </p-button>
+                    }
+                </div>
+                <div
+                    class="flex flex-col items-center justify-center mt-4 border-t-2 rounded border-gray-300 pt-4 h-50 ml-50 mr-50 bg-blue-300 w-300"
+                >
+                    <p-header>Roll Results</p-header>
+                    <p>Here are your roll results:</p>
+                </div>
 
-            <div
-                class="flex flex-col items-center justify-center mt-4 border-t-2 rounded border-gray-300 pt-4 h-50 ml-50 mr-50 bg-red-300"
-            >
-                <p-header>Recent Rolls</p-header>
-                <p>Here are your recent rolls:</p>
+                <div
+                    class="flex flex-col items-center justify-center mt-4 border-t-2 rounded border-gray-300 pt-4 h-50 ml-50 mr-50 bg-red-300 w-300"
+                >
+                    <p-header>Recent Rolls</p-header>
+                    <p>Here are your recent rolls:</p>
+                </div>
             </div>
         </p-card>
     </div>`,
