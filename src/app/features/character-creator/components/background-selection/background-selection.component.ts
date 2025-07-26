076a78270499b1f15b2f1d332e6d78c247987208
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-26 18:30:15 +0200

    feat: implement background selection component with race and background options

--- src/app/features/character-creator/components/background-selection/background-selection.component.ts
+++ src/app/features/character-creator/components/background-selection/background-selection.component.ts
@@ -1,10 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BackgroundSteps } from '@features/character-creator/dictionaries/creation-steps.dictionary';
+import { AccordionModule } from 'primeng/accordion';
+import { RaceSelectionComponent } from "./race-selection/race-selection.component";
 
 @Component({
   selector: 'app-background-selection',
-  imports: [],
+  imports: [AccordionModule, RaceSelectionComponent],
   templateUrl: './background-selection.component.html',
   styleUrl: './background-selection.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BackgroundSelectionComponent { }
+export class BackgroundSelectionComponent {
+  BackgroundSteps = BackgroundSteps;
+}
--- src/app/features/character-creator/pages/character-creator/character-creator.component.ts
+++ src/app/features/character-creator/pages/character-creator/character-creator.component.ts
@@ -1,9 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { StepperModule } from 'primeng/stepper';
-import { CreationSteps } from '../../dictionaries/creation-steps.dictionary';
 import { ButtonModule } from 'primeng/button';
 import { ClassSelectionComponent } from '../../components/class-selection/class-selection.component';
 import { BackgroundSelectionComponent } from '@features/character-creator/components/background-selection/background-selection.component';
+import { CreationSteps } from '@features/character-creator/dictionaries/creation-steps.dictionary';
 
 @Component({
     selector: 'app-character-creator',
