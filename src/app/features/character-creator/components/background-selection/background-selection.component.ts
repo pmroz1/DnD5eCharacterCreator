a725ab61ccd78e478070c3284ef831d4180db78f
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-26 18:11:16 +0200

    feat: add background selection component and integrate into character creator

---
+++ src/app/features/character-creator/components/background-selection/background-selection.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-background-selection',
+  imports: [],
+  templateUrl: './background-selection.component.html',
+  styleUrl: './background-selection.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BackgroundSelectionComponent { }
--- src/app/features/character-creator/pages/character-creator/character-creator.component.ts
+++ src/app/features/character-creator/pages/character-creator/character-creator.component.ts
@@ -2,16 +2,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { StepperModule } from 'primeng/stepper';
 import { CreationSteps } from '../../dictionaries/creation-steps.dictionary';
 import { ButtonModule } from 'primeng/button';
-import { ClassSelectionComponent } from "../../components/class-selection/class-selection.component";
-import { RaceSelectionComponent } from "@features/character-creator/components/race-selection/race-selection.component";
+import { ClassSelectionComponent } from '../../components/class-selection/class-selection.component';
+import { BackgroundSelectionComponent } from '@features/character-creator/components/background-selection/background-selection.component';
 
 @Component({
-  selector: 'app-character-creator',
-  imports: [StepperModule, ButtonModule, ClassSelectionComponent, RaceSelectionComponent],
-  templateUrl: './character-creator.component.html',
-  styleUrl: './character-creator.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-character-creator',
+    imports: [StepperModule, ButtonModule, ClassSelectionComponent, BackgroundSelectionComponent],
+    templateUrl: './character-creator.component.html',
+    styleUrl: './character-creator.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CharacterCreatorComponent {
-  CreationSteps = CreationSteps;
+    CreationSteps = CreationSteps;
 }
