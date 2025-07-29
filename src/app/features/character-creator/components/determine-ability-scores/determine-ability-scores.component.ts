a0b5335d192417f29e84f0da9def6e18d3cd1616
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-29 21:40:37 +0200

    feat: add DetermineAbilityScoresComponent for ability score determination

---
+++ src/app/features/character-creator/components/determine-ability-scores/determine-ability-scores.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-determine-ability-scores',
+  imports: [],
+  template: `<p>determine-ability-scores works!</p>`,
+  styles: '',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DetermineAbilityScoresComponent { }
