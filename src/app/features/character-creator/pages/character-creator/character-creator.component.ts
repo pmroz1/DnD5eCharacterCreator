ffc0e421129a677388cbdf626171d6d131b138a9
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-29 21:38:14 +0200

    feat: integrate DetermineAbilityScoresComponent into character creator step

--- src/app/features/character-creator/pages/character-creator/character-creator.component.ts
+++ src/app/features/character-creator/pages/character-creator/character-creator.component.ts
@@ -4,10 +4,11 @@ import { ButtonModule } from 'primeng/button';
 import { ClassSelectionComponent } from '../../components/class-selection/class-selection.component';
 import { BackgroundSelectionComponent } from '@features/character-creator/components/background-selection/background-selection.component';
 import { CreationSteps } from '@features/character-creator/dictionaries/creation-steps.dictionary';
+import { DetermineAbilityScoresComponent } from "@features/character-creator/components/determine-ability-scores/determine-ability-scores.component";
 
 @Component({
     selector: 'app-character-creator',
-    imports: [StepperModule, ButtonModule, ClassSelectionComponent, BackgroundSelectionComponent],
+    imports: [StepperModule, ButtonModule, ClassSelectionComponent, BackgroundSelectionComponent, DetermineAbilityScoresComponent],
     templateUrl: './character-creator.component.html',
     styleUrl: './character-creator.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
