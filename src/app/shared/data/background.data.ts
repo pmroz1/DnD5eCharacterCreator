0ca32214d1394c5b9dab8ac52f6a98d1a7f53369
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-29 22:05:10 +0200

    feat: add background images for Acolyte, Artisan, Criminal, Folk Hero, and others

--- src/app/shared/data/background.data.ts
+++ src/app/shared/data/background.data.ts
@@ -6,64 +6,94 @@ import { Feat } from '@shared/dictionaries/feat.dictionary';
 import { ToolProficiencies } from '@shared/dictionaries/tool-proficiences.dictionary';
 
 export const BACKGROUNDS: BackgroundModel[] = [
-    {
-        id: BackgroundDictionary.Acolyte,
-        name: BackgroundDictionary.Acolyte,
-        abilities: [Ability.Intelligence, Ability.Wisdom, Ability.Charisma],
-        skills: [Skills.Insight, Skills.Religion],
-        feats: [Feat.MagicInitiate,],
-        toolProficiencies: [ToolProficiencies.CalligraphersSupplies],
-        equipment: {
-            guaranteed: [],
-            choices: [],
-        },
-        imageUrl: 'assets/images/origins/acolyte.png',
-        description: `You devoted your life to the service in a temple, either nestled in a bustling city or secluded in a sacred grove.
-             There you performed rituals, offered guidance to the faithful, and learned the tenets of your faith.
-             You served under a priest and studied the teachings of your deity, gaining insight into the spiritual world.
-             Thanks to your priest instruction, you learned the ways of your faith and the rituals that bind it.
-            `,
+  {
+    id: BackgroundDictionary.Acolyte,
+    name: BackgroundDictionary.Acolyte,
+    abilities: [Ability.Intelligence, Ability.Wisdom, Ability.Charisma],
+    skills: [Skills.Insight, Skills.Religion],
+    feats: [Feat.MagicInitiate],
+    toolProficiencies: [ToolProficiencies.CalligraphersSupplies],
+    equipment: {
+      guaranteed: [
+        '5 sticks of incense',
+        'Vestments',
+        'Common clothes',
+        'Belt pouch containing 15 gp'
+      ],
+      choices: [
+        // 'Prayer book',
+        // 'Prayer wheel'
+      ]
     },
-    {
-        id: BackgroundDictionary.Artisan,
-        name: BackgroundDictionary.Artisan,
-        abilities: [Ability.Intelligence, Ability.Dexterity],
-        skills: [Skills.Athletics, Skills.Perception],
-        feats: [],
-        toolProficiencies: [],
-        equipment: {
-            guaranteed: [],
-            choices: [],
-        },
-        imageUrl: 'assets/images/origins/artisan.png',
-        description: 'You have honed your skills in a specific craft or trade.',
+    imageUrl: 'assets/images/origins/acolyte.png',
+    description: `You devoted your life to the service in a temple or religious order. 
+You learned the tenets of your faith and the rituals of worship, 
+and you may have even been trained in healing or other divine magic.
+You are a skilled practitioner of your faith, able to channel its power in various ways.
+`,
+  },
+  {
+    id: BackgroundDictionary.Artisan,
+    name: BackgroundDictionary.Artisan,
+    abilities: [Ability.Intelligence, Ability.Dexterity],
+    skills: [Skills.Athletics, Skills.Perception],
+    feats: [],
+    toolProficiencies: [
+    //   ToolProficiencies.ArtisansTools
+    ],
+    equipment: {
+      guaranteed: [
+        'Set of artisan’s tools (one of your choice)',
+        'Guild membership insignia',
+        'Common clothes',
+        'Belt pouch containing 15 gp'
+      ],
+      choices: []
     },
-    {
-        id: BackgroundDictionary.Criminal,
-        name: BackgroundDictionary.Criminal,
-        abilities: [Ability.Dexterity, Ability.Charisma],
-        skills: [Skills.Deception, Skills.Stealth],
-        feats: [],
-        toolProficiencies: [],
-        equipment: {
-            guaranteed: [],
-            choices: [],
-        },
-        imageUrl: 'assets/images/origins/criminal.png',
-        description: 'You have a history of illegal activities and dealings.',
+    imageUrl: 'assets/images/origins/artisan.png',
+    description: 'You have honed your skills in a specific craft or trade.',
+  },
+  {
+    id: BackgroundDictionary.Criminal,
+    name: BackgroundDictionary.Criminal,
+    abilities: [Ability.Dexterity, Ability.Charisma],
+    skills: [Skills.Deception, Skills.Stealth],
+    feats: [],
+    toolProficiencies: [
+    //   ToolProficiencies.ThievesTools,
+    //   ToolProficiencies.GamingSets
+    ],
+    equipment: {
+      guaranteed: [
+        'Crowbar',
+        'Dark common clothes',
+        'Belt pouch containing 15 gp'
+      ],
+      choices: []
     },
-    {
-        id: BackgroundDictionary.FolkHero,
-        name: BackgroundDictionary.FolkHero,
-        abilities: [Ability.Strength, Ability.Charisma],
-        skills: [Skills.AnimalHandling, Skills.Survival],
-        feats: [],
-        toolProficiencies: [],
-        equipment: {
-            guaranteed: [],
-            choices: [],
-        },
-        imageUrl: 'assets/images/origins/folk-hero.png',
-        description: 'You are a champion of the common people, known for your deeds of heroism.',
+    imageUrl: 'assets/images/origins/criminal.png',
+    description: 'You have a history of illegal activities and dealings.',
+  },
+  {
+    id: BackgroundDictionary.FolkHero,
+    name: BackgroundDictionary.FolkHero,
+    abilities: [Ability.Strength, Ability.Charisma],
+    skills: [Skills.AnimalHandling, Skills.Survival],
+    feats: [],
+    toolProficiencies: [
+    //   ToolProficiencies.ArtisansTools,
+    //   ToolProficiencies.LandVehicles
+    ],
+    equipment: {
+      guaranteed: [
+        'Shovel',
+        'Iron pot',
+        'Common clothes',
+        'Belt pouch containing 10 gp'
+      ],
+      choices: []
     },
+    imageUrl: 'assets/images/origins/folk-hero.png',
+    description: 'You are a champion of the common people, known for your deeds of heroism.',
+  },
 ];
