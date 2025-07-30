7abeb5419f3a774a566cd385e011f6b5e86b7615
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-30 21:58:05 +0200

    feat: enhance ability score determination with dynamic dice rolling and update UI components

--- src/app/features/character-creator/components/determine-ability-scores/components/ability-score-dices/ability-score-dices.component.ts
+++ src/app/features/character-creator/components/determine-ability-scores/components/ability-score-dices/ability-score-dices.component.ts
@@ -1,29 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
     selector: 'app-ability-score-dices',
     imports: [],
     template: `
-        <div
-            class="flex flex-col justify-center items-center p-ripple group cursor-pointer 
-            backdrop-blur-sm rounded-xl shadow-xl w-full flex flex-col overflow-hidden 
-            transform itemClass h-auto bg-white/10 border border-white/20"
-        >
         <div>
-          @for (diceSet of diceSets; track $index) {
-        }
+            <div class="h-full w-full grid grid-cols-3 gap-4 p-4">
+                @for (diceSet of diceSets(); track $index) {
+                <div
+                    class="flex flex-col justify-center items-center p-ripple group cursor-pointer backdrop-blur-sm rounded-xl shadow-xl w-full flex flex-col overflow-hidden transform itemClass h-auto bg-white/10 border border-white/20"
+                >
+                    <div class="text-lg font-semibold">Dice Set {{ diceSet.id }}</div>
+                    <div class="flex space-x-2">
+                        @for (roll of diceSet.rolls; track $index) {
+                        <div class="flex-1">
+                            <div class="text-center">{{ roll }}</div>
+                        </div>
+                        }
+                    </div>
+                </div>
+                }
+            </div>
         </div>
-      </div>
     `,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AbilityScoreDicesComponent {
-    diceSets = [
-        { id: 1, rolls: [0, 0, 0, 0, 0] },
-        { id: 2, rolls: [0, 0, 0, 0, 0] },
-        { id: 3, rolls: [0, 0, 0, 0, 0] },
-        { id: 4, rolls: [0, 0, 0, 0, 0] },
-        { id: 5, rolls: [0, 0, 0, 0, 0] },
-        { id: 6, rolls: [0, 0, 0, 0, 0] },
-    ];
+    diceSets = input<{ id: number; rolls: number[] }[]>();
 }
--- src/app/features/character-creator/components/determine-ability-scores/determine-ability-scores.component.ts
+++ src/app/features/character-creator/components/determine-ability-scores/determine-ability-scores.component.ts
@@ -1,26 +1,56 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { Card } from 'primeng/card';
 import { AbilityScoreDicesComponent } from './components/ability-score-dices/ability-score-dices.component';
+import { Button } from 'primeng/button';
 
 @Component({
     selector: 'app-determine-ability-scores',
-    imports: [Card, AbilityScoreDicesComponent],
+    imports: [Card, AbilityScoreDicesComponent, Button],
     template: `<div class="flex flex-col justify-center items-center w-full mx-auto mt-4">
         <p-card class="flex justify-center items-center w-full mx-auto mt-4">
             <div class="flex flex-col justify-center items-center">
                 <p
-                    class="text-3xl font-bold mb-4 drop-shadow-lg transition-colors duration-300 text-white"
+                    class="text-5xl font-bold mb-4 drop-shadow-lg transition-colors duration-300 text-white"
                 >
                     Determine Ability Scores
                 </p>
-                <p class="text-xl">1) Roll 4d6 and drop the lowest die</p>
+                <p class="text-xl">1) Roll 4d6 and discard the lowest die</p>
                 <p class="text-xl">2) Repeat for each ability score for total of 6 scores</p>
                 <p class="text-xl">3) Assign scores to abilities as desired</p>
+                <p-button class="pt-5 pb-3" (click)="rollPoints()">Roll points</p-button>
             </div>
-            <app-ability-score-dices class="mt-6"></app-ability-score-dices>
+            <app-ability-score-dices [diceSets]="diceSets()" class="mt-6"></app-ability-score-dices>
         </p-card>
     </div>`,
     styles: '',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DetermineAbilityScoresComponent {}
+export class DetermineAbilityScoresComponent {
+    diceSets = signal([
+        { id: 1, rolls: [6, 6, 6, 6] },
+        { id: 2, rolls: [6, 6, 6, 6] },
+        { id: 3, rolls: [6, 6, 6, 6] },
+        { id: 4, rolls: [6, 6, 6, 6] },
+        { id: 5, rolls: [6, 6, 6, 6] },
+        { id: 6, rolls: [6, 6, 6, 6] },
+    ]);
+
+    rollPoints() {
+        this.diceSets.update((set) => {
+            return set.map((diceSet) => {
+                const newRolls = this.rollDice();
+                return { ...diceSet, rolls: newRolls };
+            });
+        });
+    }
+
+    rollDice() {
+        const rolls = [];
+        for (let i = 0; i < 4; i++) {
+            rolls.push(Math.floor(Math.random() * 6) + 1);
+        }
+        rolls.sort((a, b) => b - a);
+        rolls.pop();
+        return rolls;
+    }
+}
