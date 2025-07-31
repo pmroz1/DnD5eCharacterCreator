38cad3b3bff5045deb9cf1b5c7b3bd95b0a1a708
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-31 21:38:57 +0200

    feat: enhance ability points display in AssignAbilityPointsComponent

--- src/app/features/character-creator/components/determine-ability-scores/components/assign-ability-points/assign-ability-points.component.ts
+++ src/app/features/character-creator/components/determine-ability-scores/components/assign-ability-points/assign-ability-points.component.ts
@@ -4,10 +4,26 @@ import { AbilityAssignMap } from '../../models/ability-assign.map';
 @Component({
     selector: 'app-assign-ability-points',
     imports: [JsonPipe],
-    template: `{{ abilityPointsMap() | json}}
-        <div class="flex flex-col justify-center items-center w-full mx-auto mt-4"></div>`,
+    template: `{{ abilityPointsMap() | json }}
+        <div class="flex flex-col justify-center items-center w-full mx-auto mt-4">
+            @if(abilityPointsMap() !== null && abilityPointsMap() !== undefined) { @for(map of
+            objectKeys(abilityPointsMap()); track map) {
+            <div
+                class="flex justify-between w-full max-w-md p-4 bg-gray-100 rounded-lg shadow-md mb-2"
+            >
+                <span class="font-semibold text-lg">{{ map }}</span>
+                <span class="text-xl font-bold">{{ abilityPointsMap()?.[map] }}</span>
+            </div>
+            }} @else {
+            <p class="text-gray-500">No ability points assigned yet.</p>
+            }
+        </div>`,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AssignAbilityPointsComponent {
     abilityPointsMap = input<AbilityAssignMap>();
+
+    objectKeys(obj: AbilityAssignMap | null | undefined): string[] {
+        return Object.keys(obj || {});
+    }
 }
