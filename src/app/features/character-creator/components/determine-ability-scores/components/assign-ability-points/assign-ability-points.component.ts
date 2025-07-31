3aacd91c7c05b7255d1b854d0987f0c161d4d2b3
DESKTOP-3TNQ0PC\Piotr <p.mroz.zywiol@gmail.com>
2025-07-31 22:13:14 +0200

    feat: enhance layout and styling in AssignAbilityPointsComponent template

--- src/app/features/character-creator/components/determine-ability-scores/components/assign-ability-points/assign-ability-points.component.ts
+++ src/app/features/character-creator/components/determine-ability-scores/components/assign-ability-points/assign-ability-points.component.ts
@@ -4,7 +4,7 @@ import { ChartModule } from 'primeng/chart';
 @Component({
     selector: 'app-assign-ability-points',
     imports: [ChartModule],
-    template: ` <div class="flex flex-row gap-2">
+    template: ` <div class="flex flex-row gap-2 justify-between items-center p-4">
         <div class="flex flex-col justify-center items-center w-full mx-auto mt-4">
             @if(abilityPointsMap() !== null && abilityPointsMap() !== undefined) { @for(map of
             objectKeys(abilityPointsMap()); track map) {
@@ -20,7 +20,7 @@ import { ChartModule } from 'primeng/chart';
             <p class="text-gray-500">No ability points assigned yet.</p>
             }
         </div>
-        <div class="card flex justify-center">
+        <div class="card flex justify-center w-full ">
             <p-chart type="radar" [data]="data" [options]="options" class="w-full md:w-[30rem]" />
         </div>
     </div>`,
